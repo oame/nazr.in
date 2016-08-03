aa45ad72a970156f1103d0869f0bec6989ad3920
oame <oame@oameya.com>
2016-08-03 13:48:04 +0900

    Add Google Analytics

--- client/index.jsx
+++ client/index.jsx
@@ -1,8 +1,11 @@
 import './stylus/index.styl'
 import React from 'react'
 import {render} from 'react-dom'
+import ReactGA from 'react-ga'
 
 import App from './components/app'
+
+ReactGA.initialize('UA-28919359-3')
 
 render(
 	<App/>, document.getElementById('root')
