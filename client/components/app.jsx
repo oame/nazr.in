14c8de1a4f95e54f43b56a296c64850053be054f
oame <oame@oameya.com>
2016-11-21 12:30:49 +0900

    chore: fix typo

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -6,8 +6,8 @@ export default () => {
 	const links = [
 		{title: 'GitHub', url: 'https://github.com/oame/nazr.in'},
 		{title: '@o_ame (コード)', url: 'https://twitter.com/o_ame'},
-		{title: '@kahasina9 (ロゴデザイン)', url: 'https://twitter.com/kahasina9'},
-		{title: 'ZUN / 上海アリス幻樂団 (原作)', url: 'http://www16.big.or.jp/~zun/}'}
+		{title: '@kahasina9 (ロゴ)', url: 'https://twitter.com/kahasina9'},
+		{title: 'ZUN / 上海アリス幻樂団 (原作)', url: 'http://www16.big.or.jp/~zun/'}
 	];
 
 	return (
