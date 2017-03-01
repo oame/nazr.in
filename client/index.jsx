c06cd80419c0f490c1f4a0ee1da99d7aac0c02e5
oame <oame@oameya.com>
2017-03-01 17:58:07 +0900

    feat: add raven-js

--- client/index.jsx
+++ client/index.jsx
@@ -3,10 +3,12 @@ import './stylus/index.styl'
 import React from 'react'
 import {render} from 'react-dom'
 import ReactGA from 'react-ga'
+import Raven from 'raven-js'
 
 import App from './components/app'
 
 ReactGA.initialize('UA-28919359-3')
+Raven.config('https://1900792671ca4e2f9cf697717e07ff41@sentry.io/143585').install()
 
 render(
   <App />, document.getElementById('root')
